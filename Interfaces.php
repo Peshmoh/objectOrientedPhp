d61c4c6e29da85fe3378472e6806f66426ed20a1
peris <perismumbi96@gmail.com>
2024-10-01 21:34:26 +0300

    created description about wats interfaces are and the examples

--- Interfaces.php
+++ Interfaces.php
@@ -1,10 +1,17 @@
 <?php
 /*
-    An interface is like a plan. Classes that use the interface must follow the plan 
-    and create the methods defined in the interface. This example shows how Mpesa 
-    and BankTransfer can both have a sendMoney method because they use the Payment interface.
-*/
+    what I understood about interfaces is that they’re like a set of rules or a plan 
+    that classes have to follow. It’s kind of like if we all agreed on the same way 
+    to do something, but each person can do it a little differently.
 
+    In this case, the interface is called Payment, and it has one rule: 
+    any class that uses Payment must have a sendMoney method.
+
+    So, Mpesa and BankTransfer both agree to follow the Payment plan, 
+    but they send money in their own ways. Mpesa sends money using mobile phones, 
+    while BankTransfer sends money through the bank. 
+    Even though they do it differently, they both follow the same rule – they both have to use sendMoney.
+*/
 interface Payment {
     public function sendMoney($amount);
 }
